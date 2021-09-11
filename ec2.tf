1699f71085046f23b726236260f61e938b7d9695
Ademilton Cavalcante <ademilton.cavalcante>
2021-09-11 16:06:23 -0300

    arquivos formatados

--- ec2.tf
+++ ec2.tf
@@ -1,13 +1,13 @@
 resource "aws_instance" "server" {
-  ami = var.ami
+  ami           = var.ami
   instance_type = var.instance_type
 
-  tags = { 
-    Name = var.name
+  tags = {
+    Name        = var.name
     Environment = var.env
     Provisioner = "Terraform"
-    Repo = var.source
-    
+    Repo        = var.source
+
   }
 
 }
--- variables.tf
+++ variables.tf
@@ -1,35 +1,35 @@
 variable "region" {
   description = "Define what region the instance will be deployed"
-  default = "us-east-1"
+  default     = "us-east-1"
 
 }
 
 
 variable "name" {
   description = "Name of the application"
-  default = "server01"
+  default     = "server01"
 
 }
 
 
 variable "env" {
   description = "Environment of the application"
-  default = "dev"
+  default     = "dev"
 }
 
 variable "ami" {
   description = "AWS AMI to be used"
-  default = "ami-042e8287309f5df03"
+  default     = "ami-042e8287309f5df03"
 
 }
 variable "instance_type" {
   description = "AWS Instance type defines the hardware configuration of the machine"
-  default = "t2.micro"
+  default     = "t2.micro"
 }
 
 variable "repo" {
-    description = "Repository of the application"
-    default = "https://github.com/ademiltonc/terraformaws"
-    
-  
+  description = "Repository of the application"
+  default     = "https://github.com/ademiltonc/terraformaws"
+
+
 }
