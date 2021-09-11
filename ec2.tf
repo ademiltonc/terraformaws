17ff0e1d5a597dae926c408a3b47ef3028a44af5
Ademilton Cavalcante <ademilton.cavalcante>
2021-09-10 22:10:31 -0300

    initial commit

---
+++ ec2.tf
@@ -0,0 +1,13 @@
+resource "aws_instance" "server" {
+  ami = var.ami
+  instance_type = var.instance_type
+
+  tags = { 
+    Name = var.name
+    Environment = var.env
+    Provisioner = "Terraform"
+    Repo = var.source
+    
+  }
+
+}
